b9800a68549ce11f945f4446bd4131b4165dea76
devsgnr <hi@devsgnr.xyz>
2022-09-06 23:16:04 +0100

    Initial Commit

---
+++ NASA/NASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAApp.swift
+//  NASA
+//
+//  Created by Emmanuel Watila on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct NASAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
